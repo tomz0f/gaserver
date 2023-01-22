4473bc55160d408204ecbd9bd02c7026023ba967
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-01-23 00:58:32 +0300

    gadata

--- gadata/train-ai/ai.py
+++ gadata/train-ai/ai.py
@@ -2,6 +2,6 @@ from imageai.Classification.Custom import ClassificationModelTrainer
 
 model_trainer = ClassificationModelTrainer()
 model_trainer.setModelTypeAsResNet50()
-model_trainer.setDataDirectory("idenprof")
+model_trainer.setDataDirectory("galari")
 model_trainer.trainModel(num_experiments=200, batch_size=32)
 
